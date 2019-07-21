2fba32247a298ce7c75f62067c7418f47b4ab82c
alvinjtang <alvintangdesigns@gmail.com>
2019-07-21 08:22:24 -0400

    set up components (Game, Board, Square)

---
+++ src/games/TicTacToe/components/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Board from './Board';
+
+export default () => {
+    return (
+        <div id='game'>
+            <div id='board'>
+                <Board />
+            </div>
+        </div>
+    )
+}
--- src/games/TicTacToe/index.js
+++ src/games/TicTacToe/index.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import {Game} from './components'
 
 export default class TicTacToe extends Component {
     render() {
         return (
             <div>
-                TicTacToe
+                <Game />
             </div>
         )
     }
